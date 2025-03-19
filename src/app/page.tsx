af3c75822112f55f71290d1837ea9ef926b13f36
anshumangadha <anshuman@gmail.com>
2025-03-19 13:01:29 +0530

    se

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -170,7 +170,7 @@ export default function Home() {
         {loading ? (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {[1, 2, 3, 4, 5, 6].map((item) => (
-              <Card key={item} className="h-[350px] animate-pulse">
+              <Card key={item} className="h-56 w-56 animate-pulse">
                 <div className="h-full bg-gray-200"></div>
               </Card>
             ))}
